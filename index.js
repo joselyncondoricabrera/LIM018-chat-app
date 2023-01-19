b55f9369c6dd528fa3776e11f429b863652c80b6
joselyncondoricabrera <condori.cabrera.joselyn@gmail.com>
2023-01-19 12:00:18 -0500

    conexion de node con postgresql y insert y select de tablas

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Pool } = require('pg');
+
+const config = {
+  user: 'postgres',
+  host: 'localhost',
+  password: 'postgres',
+  database: 'postgres',
+};
+const pool = new Pool(config);
+
+const getUser = async () => {
+  try {
+    const res = await pool.query('select * from usuario');
+    console.log(res.rows);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const insertUser = async () => {
+  try {
+    const text =
+      'INSERT INTO usuario(idusuario,nombre,correo,contraseña) VALUES($1, $2, $3, $4)';
+    const values = [2, 'Jackelin', 'jacki@gmail.com', '123456'];
+
+    const res = pool.query(text, values);
+    console.log(res);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+insertUser();
+getUser();
